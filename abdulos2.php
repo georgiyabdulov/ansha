a6545dde0123f100585e1c516ebd84ec1d8292e7
georgiyabdulov <georgykozlov2206@gmail.com>
2025-05-14 20:35:08 +0500

    Update abdulos2.php

--- abdulos2.php
+++ abdulos2.php
@@ -49,10 +49,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
     //считаю количество предложений, пробую без гпт//
     $elementCount = count($matchingSentences);
-        $elementCount() = 3 || $elementCount() = 4
+        if ($elementCount == 3 || $elementCount == 4 || $elementCount == 2){
         echo "Абдуль уже говорил так " . $elementCount . ' раза';
-        else
+    } else {
         echo "Абдуль уже говорил так " . $elementCount . ' раз';
+    }
     if (!empty($sentence14) && !empty($userInput)) {
         $randomSubstring = $sentence14[array_rand($sentence14)];
     } else {
