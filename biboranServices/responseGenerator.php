2dc1feea25302dc26c1ef0028101a41b3e8f8713
georgiyabdulov <georgykozlov2206@gmail.com>
2025-05-19 20:17:15 +0500

    вввв

--- biboranServices/responseGenerator.php
+++ biboranServices/responseGenerator.php
@@ -1,2 +1,16 @@
 <?php 
+function GenerateResponse (string $randomSubstring, array $matchingSentences)
+ $matchingSentences = [];
+    $randomSubstring = '';
+    $userInput = $_POST['user_input'] ?? '';
+    $userWords = preg_split('/\s+/', strtolower(trim($userInput)));
 
+    foreach ($sentences as $sentence) {
+        $lowerSentence = strtolower($sentence);
+        foreach ($userWords as $word) {
+            if ($word && strpos($lowerSentence, $word) !== false) {
+                $matchingSentences[] = $sentence;
+                break;
+            }
+        }
+    }
